5c8a47b3c7e39d76a300a30698ff769d12214288
Nick-Lemy <n.kayiranga@alustudent.com>
2025-09-13 19:00:32 +0200

    commit

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-if __name__ == "__main__":
-    def matrix_shape(matrix):
-        shape = []
-        while isinstance(matrix, list):
-            shape.append(len(matrix))
-            if len(matrix) == 0:
-                break
-            matrix = matrix[0]
-        return shape
+def matrix_shape(matrix):
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        if len(matrix) == 0:
+            break
+        matrix = matrix[0]
+    return shape
