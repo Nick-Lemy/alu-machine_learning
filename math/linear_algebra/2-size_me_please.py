7edd2cb4f3a368333b6ff48d12214072561bdc5a
Nick-Lemy <n.kayiranga@alustudent.com>
2025-09-13 18:58:51 +0200

    commit

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
-import numpy as np
+
+from matplotlib.pylab import shape
+
+
 if __name__ == "__main__":
     def matrix_shape(matrix):
-        return np.shape(matrix)
+        shape = []
+        while isinstance(matrix, list):
+            shape.append(len(matrix))
+            if len(matrix) == 0:
+                break
+            matrix = matrix[0]
+        return shape
