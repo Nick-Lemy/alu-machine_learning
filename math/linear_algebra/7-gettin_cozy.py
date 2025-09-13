3196f04646d0b7b4660c75da0d9e8cfde19cc953
Nick-Lemy <n.kayiranga@alustudent.com>
2025-09-13 19:55:12 +0200

    commit

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -9,6 +9,10 @@ def cat_matrices2D(mat1, mat2, axis=0):
     Args:
         matrix (list): _description_
     """
+    if axis == 0 and len(mat1[0]) != len(mat2[0]):
+        return None
+    if axis == 1 and len(mat1) != len(mat2):
+        return None
     if axis == 0:
         result = []
         for elem in mat1:
@@ -21,5 +25,3 @@ def cat_matrices2D(mat1, mat2, axis=0):
         for i in range(len(mat1)):
             result.append(mat1[i] + mat2[i])
         return result
-    else:
-        return None
